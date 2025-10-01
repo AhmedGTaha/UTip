228ac0926e00bc712d350efebff19ff0924d5815
AhmedGTaha <ahmedtahaghodhbani@gmail.com>
2025-10-01 21:17:18 +0100

    add incrementPerson and decrementPerson func

--- lib/main.dart
+++ lib/main.dart
@@ -27,10 +27,23 @@ class UTip extends StatefulWidget {
 }
 
 class _UTipState extends State<UTip> {
-  @override
+  int personCount = 1;
+  //Mthods
+  void incrementPerson() {
+    setState(() {
+      personCount = personCount + 1;
+    });
+  }
+
+  void decrementPerson() {
+    setState(() {
+      if (personCount > 0) personCount = personCount - 1;
+    });
+  }
+
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    //add style
+    //add style to text
     final style = theme.textTheme.titleMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
       fontWeight: FontWeight.bold,
@@ -46,7 +59,7 @@ class _UTipState extends State<UTip> {
               padding: const EdgeInsets.all(8.0),
               decoration: BoxDecoration(
                 color: theme.colorScheme.inversePrimary,
-                borderRadius: BorderRadius.circular(8),
+                borderRadius: BorderRadius.circular(8.0),
               ),
               child: Column(
                 children: [
@@ -57,6 +70,65 @@ class _UTipState extends State<UTip> {
                       color: theme.colorScheme.onPrimary,
                       fontSize: theme.textTheme.displaySmall?.fontSize,
                     ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          //Form
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: theme.colorScheme.primary, width: 2),
+              ),
+              child: Column(
+                children: [
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    onChanged: (String value) {
+                      print(value);
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Bill Amount',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(
+                        Icons.attach_money,
+                        color: theme.colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                  //Split bill section
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Split', style: theme.textTheme.titleMedium),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              if (personCount > 1) decrementPerson();
+                            },
+                            icon: Icon(
+                              Icons.remove_circle,
+                              color: theme.colorScheme.primary,
+                            ),
+                          ),
+                          Text(
+                            "$personCount",
+                            style: theme.textTheme.titleMedium,
+                          ),
+                          IconButton(
+                            onPressed: incrementPerson,
+                            icon: Icon(
+                              Icons.add_circle,
+                              color: theme.colorScheme.primary,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ],
               ),
